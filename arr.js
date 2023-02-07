dc1ee1442bd760dd325ecadd4b11f8fe38c4d2e3
Shanto57575 <shanto57575@gmail.com>
2023-02-07 22:16:39 +0600

    done1

--- arr.js
+++ arr.js
@@ -3,7 +3,7 @@
    This function takes an array as input and returns how many negative number exists in array!
 */
 function findingBadData(numericArray){
-   if(typeof(numericArray)!='object') return "Input should be an array";
+   if(Array.isArray(numericArray)==false) return "Input should be an array";
    let badData = 0;
    for(let i=0; i<numericArray.length; i++){
      if(numericArray[i]<0)badData++;
@@ -12,4 +12,4 @@ function findingBadData(numericArray){
 }
 
 let arr =  [ 4, 9, -5, -33, -55 ];
-console.log(findingBadData(arr));
+console.log(findingBadData("s"));
